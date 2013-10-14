2b180d04b7a7e1102a7c90e1db3bca816b53e0d4
Joris de Vries <joris@apptrician.nl>
2013-10-14 18:33:24 +0200

    ar71xx: Move WNDR4300 from the generic target to NAND-subtarget
    By moving the WNDR4300 to the NAND-subtarget, the correct image will
    be created immediately.

--- target/linux/ar71xx/generic/profiles/netgear.mk
+++ target/linux/ar71xx/generic/profiles/netgear.mk
@@ -27,19 +27,6 @@ endef
 
 $(eval $(call Profile,WNDR3700))
 
-
-define Profile/WNDR4300
-	NAME:=NETGEAR WNDR4300
-	PACKAGES:=kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-ledtrig-usbdev
-endef
-
-define Profile/WNDR4300/Description
-	Package set optimized for the NETGEAR WNDR4300
-endef
-
-$(eval $(call Profile,WNDR4300))
-
-
 define Profile/WNR2000V3
 	NAME:=NETGEAR WNR2000V3
 endef
